2f3470a8b27819ff47d9e03b931bd97d01888102
Ronyson <ronyson.silva96@gmail.com>
2024-02-06 10:22:00 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ import 'package:flutter_launcher_icons/web/web_template.dart';
 import 'package:flutter_launcher_icons/windows/windows_icon_generator.dart';
 import 'package:flutter_launcher_icons/xml_templates.dart';
 import 'package:responsive_builder/responsive_builder.dart';
-
+// rony burro
 import 'componentes/theme/theme.dart';
 
 void main() async {
