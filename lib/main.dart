2e4c25edfc379e2c27824c395cb6de4a7dd4a74a
Ronyson <ronyson.silva96@gmail.com>
2024-02-06 10:43:16 -0300

    Update main.dart
    teste de descrição

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ import 'package:flutter_launcher_icons/web/web_template.dart';
 import 'package:flutter_launcher_icons/windows/windows_icon_generator.dart';
 import 'package:flutter_launcher_icons/xml_templates.dart';
 import 'package:responsive_builder/responsive_builder.dart';
-
+/// decri
 
 import 'componentes/theme/theme.dart';
 
