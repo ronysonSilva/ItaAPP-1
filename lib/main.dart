c2a6af6f53b4b1da9d25bbea6b06ef20fd7e374a
Tiago Almeida <108297008+Wfelipetm@users.noreply.github.com>
2024-02-06 10:49:26 -0300

    001
    inicio do dia 06/02

--- lib/main.dart
+++ lib/main.dart
@@ -34,13 +34,6 @@ import 'package:flutter_launcher_icons/windows/windows_icon_generator.dart';
 import 'package:flutter_launcher_icons/xml_templates.dart';
 import 'package:responsive_builder/responsive_builder.dart';
 
-// teste burro
-/// barra barra barra
-// teste mediano
-
-//fiz merda no seu
-//merda 23
-
 import 'componentes/theme/theme.dart';
 
 void main() async {
